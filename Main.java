d0f1aeeb0e304361fcfd1bbd15898f8d44bfb9e9
Harshada1168 <106173153+Harshada1168@users.noreply.github.com>
2022-11-15 22:29:35 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,14 +8,14 @@ public class Main {
         System.out.println("Test Case 1: ");
         System.out.println("Cost of the tree is: ");
         System.out.println(o.OptimalBinarySearchTree(arr, cost, n));
-
+        // test case 2:
         int arr1[] = { 12, 13, 23, 14, 5 };
         int cost1[] = { 3, 2, 34, 12, 45 };
         int n1 = arr1.length;
         System.out.println("Test Case 2: ");
         System.out.println("Cost of the tree is: ");
         System.out.println(o.OptimalBinarySearchTree(arr1, cost1, n1));
-
+        // test case 3:
         int arr2[] = { 10, 12, 90, 45, 34, 23 };
         int cost2[] = { 1, 2, 3, 4, 5, 6 };
         int n2 = arr2.length;
